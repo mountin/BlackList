85266bd6ffb88fce1e94daddd9e539ad0b677dec
Sergii cha <mountin2@gmail.com>
2015-06-08 23:33:22 +0300

    first commit2

---
+++ app/src/main/java/com/blacklist/start/blacklist/StopListActivity.java
@@ -0,0 +1,59 @@
+package com.blacklist.start.blacklist;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class StopListActivity extends ListActivity implements AdapterView.OnItemLongClickListener {
+
+    final String[] catNamesArray = new String[]{"88888", "567585", "44444",
+            "2222222", "234235623", "000000", "222222", "888444", "56546547",
+            "1111111111", "+38908678432", "+789412456"};
+
+    private ArrayAdapter<String> mAdapter;
+    private ArrayList<String> catNamesList = new ArrayList<>(Arrays.asList(catNamesArray));
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        mAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, catNamesList);
+        setListAdapter(mAdapter);
+        getListView().setOnItemLongClickListener(this);
+        setContentView(R.layout.activity_stoplist);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+//        Toast.makeText(getApplicationContext(),
+//                "Вы выбрали " + (position + 1) + " элемент", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(getApplicationContext(),
+                "you selected " + l.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        String selectedItem = parent.getItemAtPosition(position).toString();
+
+        mAdapter.remove(selectedItem);
+        mAdapter.notifyDataSetChanged();
+
+        Toast.makeText(getApplicationContext(),
+                selectedItem + " deleted.",
+                Toast.LENGTH_SHORT).show();
+        return true;
+    }
+}
